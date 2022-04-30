633e0d0ea0fc29ca897e4bc4c02f538fb4fb814d
molu <2279126672@qq.com>
2022-04-30 20:21:02 +0800

    第一次 提交

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("aaa test");
+    }
+}
